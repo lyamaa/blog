b66df0a3194b8e158c6d3bc7cedb691d607aeb29
surajmt8848 <you@example.com>
2021-04-24 12:04:52 +0545

    add authenticationclass

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
 }
 
 
