0547de10d12140dbab089945bd5a212e55dcd021
surajmt8848 <you@example.com>
2021-04-24 12:06:36 +0545

    add jwt urls

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('blog.urls')),
     # path('api/', include("blog_api.urls")),
     path('api/', include("blog.api.urls", namespace='blog_api')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
