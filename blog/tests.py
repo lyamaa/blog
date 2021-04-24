ade6f3c609b1e92a8d6efe4c1ed57f67b43a94c7
surajmt8848 <you@example.com>
2021-04-24 11:39:40 +0545

    test post create and update

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
+
 from django.contrib.auth.models import User
 from blog.models import Post, Category
 
@@ -56,4 +58,36 @@ class PostTests(APITestCase):
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    
+    def test_post_update(self):
+
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+        self.testuser2 = User.objects.create_user(
+            username='test_user2', password='123456789')
+        test_post = Post.objects.create(
+            category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+
+        client.login(username=self.testuser1.username,
+                     password='123456789')
+
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+
+        response = client.put(
+            url, {
+                "title": "New",
+                "author": 1,
+                "slug": "abcd",
+                "excerpt": "New",
+                "content": "New",
+                "status": "published"
+            }, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
 
